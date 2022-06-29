7e3244c7adb94fcb1c726417107a0bbe9df25fa9
Oleksandr Oleniuk <sanya.olenyuk@gmail.com>
2022-06-29 17:48:52 +0100

    Change tsconfig type

--- tooling/src/builders/src/xliffmergebuilder/xliffmerge.builder_spec.js
+++ tooling/src/builders/src/xliffmergebuilder/xliffmerge.builder_spec.js
@@ -1,7 +1,9 @@
-import * as tslib_1 from "tslib";
-import { join, normalize, virtualFs } from '@angular-devkit/core';
-import { createArchitect, host } from './testing_utils';
-import { TestLogger } from './testlogger_spec';
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const tslib_1 = require("tslib");
+const core_1 = require("@angular-devkit/core");
+const testing_utils_1 = require("./testing_utils");
+const testlogger_spec_1 = require("./testlogger_spec");
 describe('xliffmerge.builder', () => {
     let architect;
     const xliffmergeTargetSpec = { project: 'hello-world-app', target: 'xliffmerge' };
@@ -18,15 +20,15 @@ describe('xliffmerge.builder', () => {
          * We are using a test workspace from the test folder.
          * In this workspace the xliffmerge builder is already configured.
          */
-        yield host.initialize().toPromise();
-        const architectInfo = yield createArchitect(host.root());
+        yield testing_utils_1.host.initialize().toPromise();
+        const architectInfo = yield testing_utils_1.createArchitect(testing_utils_1.host.root());
         architect = architectInfo.architect;
     }));
     afterEach(() => tslib_1.__awaiter(this, void 0, void 0, function* () {
-        host.restore().toPromise();
+        testing_utils_1.host.restore().toPromise();
     }));
     it('should show error when called with illegal profile', () => tslib_1.__awaiter(this, void 0, void 0, function* () {
-        const logger = new TestLogger('logger');
+        const logger = new testlogger_spec_1.TestLogger('logger');
         const profileName = 'nonexistentfile';
         const builderOutput = yield runXliffmergeBuilderOnTestWorkspace({ profile: profileName }, logger);
         expect(builderOutput.success).toBe(false);
@@ -35,7 +37,7 @@ describe('xliffmerge.builder', () => {
         expect(logger.includes(profileName)).toBe(true, `filename "${profileName}" not found in log`);
     }));
     it('should show error when called with illegal configuration', () => tslib_1.__awaiter(this, void 0, void 0, function* () {
-        const logger = new TestLogger('logger');
+        const logger = new testlogger_spec_1.TestLogger('logger');
         const xlfFileName = 'nonexistentxlffile';
         const builderOutput = yield runXliffmergeBuilderOnTestWorkspace({ xliffmergeOptions: { i18nFile: xlfFileName } }, logger);
         expect(builderOutput.success).toBe(false);
@@ -44,7 +46,7 @@ describe('xliffmerge.builder', () => {
         expect(logger.includes(xlfFileName)).toBe(true, `filename "${xlfFileName}" not found in log`);
     }));
     it('should use profile when called with both profile and configuration', () => tslib_1.__awaiter(this, void 0, void 0, function* () {
-        const logger = new TestLogger('logger');
+        const logger = new testlogger_spec_1.TestLogger('logger');
         const profileName = 'nonexistentfile';
         const xlfFileName = 'nonexistentxlffile';
         const builderOutput = yield runXliffmergeBuilderOnTestWorkspace({
@@ -56,7 +58,7 @@ describe('xliffmerge.builder', () => {
         expect(logger.includes(profileName)).toBe(true, `filename "${profileName}" not found in log`);
     }));
     it('should run successfully with given xliffmergeOptions', () => tslib_1.__awaiter(this, void 0, void 0, function* () {
-        const logger = new TestLogger('logger');
+        const logger = new testlogger_spec_1.TestLogger('logger');
         const configuration = {
             xliffmergeOptions: {
                 'srcDir': 'src/i18n',
@@ -65,19 +67,19 @@ describe('xliffmerge.builder', () => {
                 verbose: true
             }
         };
-        const generatedFileEN = join(normalize('src'), 'i18nout', 'messages.en.xlf');
-        const generatedFileDE = join(normalize('src'), 'i18nout', 'messages.de.xlf');
+        const generatedFileEN = core_1.join(core_1.normalize('src'), 'i18nout', 'messages.en.xlf');
+        const generatedFileDE = core_1.join(core_1.normalize('src'), 'i18nout', 'messages.de.xlf');
         const builderOutput = yield runXliffmergeBuilderOnTestWorkspace(configuration, logger);
         expect(builderOutput.success).toBe(true);
         const msg = 'WARNING: please translate file';
         expect(logger.includes(msg)).toBe(true, `msg "${msg}" not found in log`);
-        host.scopedSync().read(generatedFileEN);
-        host.scopedSync().read(generatedFileDE);
-        expect(yield host.scopedSync().exists(generatedFileEN)).toBe(true, `file ${generatedFileEN} not generated`);
-        expect(yield host.scopedSync().exists(generatedFileDE)).toBe(true, `file ${generatedFileDE} not generated`);
+        testing_utils_1.host.scopedSync().read(generatedFileEN);
+        testing_utils_1.host.scopedSync().read(generatedFileDE);
+        expect(yield testing_utils_1.host.scopedSync().exists(generatedFileEN)).toBe(true, `file ${generatedFileEN} not generated`);
+        expect(yield testing_utils_1.host.scopedSync().exists(generatedFileDE)).toBe(true, `file ${generatedFileDE} not generated`);
     }));
     it('should run successfully with options from profile', () => tslib_1.__awaiter(this, void 0, void 0, function* () {
-        const logger = new TestLogger('logger');
+        const logger = new testlogger_spec_1.TestLogger('logger');
         const profileContent = {
             xliffmergeOptions: {
                 'srcDir': 'src/i18n',
@@ -85,18 +87,18 @@ describe('xliffmerge.builder', () => {
                 languages: ['en', 'de']
             }
         };
-        host.scopedSync().write(join(normalize('.'), 'xliffmergeconfig.json'), virtualFs.stringToFileBuffer(JSON.stringify(profileContent)));
+        testing_utils_1.host.scopedSync().write(core_1.join(core_1.normalize('.'), 'xliffmergeconfig.json'), core_1.virtualFs.stringToFileBuffer(JSON.stringify(profileContent)));
         const configuration = {
             profile: 'xliffmergeconfig.json'
         };
-        const generatedFileEN = join(normalize('src'), 'i18nout', 'messages.en.xlf');
-        const generatedFileDE = join(normalize('src'), 'i18nout', 'messages.de.xlf');
+        const generatedFileEN = core_1.join(core_1.normalize('src'), 'i18nout', 'messages.en.xlf');
+        const generatedFileDE = core_1.join(core_1.normalize('src'), 'i18nout', 'messages.de.xlf');
         const builderOutput = yield runXliffmergeBuilderOnTestWorkspace(configuration, logger);
         expect(builderOutput.success).toBe(true);
         const msg = 'WARNING: please translate file';
         expect(logger.includes(msg)).toBe(true, `msg "${msg}" not found in log`);
-        expect(host.scopedSync().exists(generatedFileEN)).toBe(true, `file ${generatedFileEN} not generated`);
-        expect(host.scopedSync().exists(generatedFileDE)).toBe(true, `file ${generatedFileDE} not generated`);
+        expect(testing_utils_1.host.scopedSync().exists(generatedFileEN)).toBe(true, `file ${generatedFileEN} not generated`);
+        expect(testing_utils_1.host.scopedSync().exists(generatedFileDE)).toBe(true, `file ${generatedFileDE} not generated`);
     }));
 });
 //# sourceMappingURL=xliffmerge.builder_spec.js.map
